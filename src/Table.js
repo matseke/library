32d0cf787d8e4106e7863fb1f9d68522d3e6934c
Mats Ekelund <mats.ekelund@fro.se>
2020-10-07 13:04:58 +0200

    Sortable table

--- src/Table.js
+++ src/Table.js
@@ -1,19 +1,35 @@
-import React, {Component} from 'react';
-import TableHead from './TableHead';
+import React from 'react';
 import TableBody from './TableBody';
 
-class Table extends Component {
-    render() {
-        const {libraryData} = this.props;
-        return(
-            <React.Fragment>
+const Table = (props) => {
+    const [sortedField, setSortedField] = React.useState(null);
+    const {libraryData} = props;
+    let sortedData = [...libraryData];
+    if (sortedField !== null) {
+        sortedData.sort((a, b) => {
+        if (a[sortedField] < b[sortedField]) {
+          return -1;
+        }
+        if (a[sortedField] > b[sortedField]) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+   
+    return(
+        <React.Fragment>
             <table>
-                    <TableHead/>
-                    <TableBody libraryData = {libraryData}/>
+                <thead>
+                    <tr>
+                        <th><button type="button" onClick={() => setSortedField('title')}>Title</button></th>
+                        <th><button type="button" onClick={() => setSortedField('author')}>Author</button></th>
+                    </tr>
+                </thead>
+                <TableBody libraryData = {sortedData}/>
             </table>
         </React.Fragment>
-        )
-    }
+    )
 }
 
 export default Table;
