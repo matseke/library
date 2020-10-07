da013f4179632215b661c03eee82e448bb07888b
Mats Ekelund <mats.ekelund@fro.se>
2020-10-07 13:57:49 +0200

    Memoization

--- src/Table.js
+++ src/Table.js
@@ -5,19 +5,23 @@ const Table = (props) => {
     const [sortConfig, setSortConfig] = React.useState(null);
     const {libraryData} = props;
 
-    var sortedData = [...libraryData];
+    var tableData = React.useMemo(() => {
+        var sortedData = [...libraryData];
 
-    if (sortConfig != null) {
-        sortedData.sort((a, b) => {
-            if (a[sortConfig.key] < b[sortConfig.key]) {
-                return sortConfig.direction === 'ascending' ? -1 : 1;
-            }
-            if (a[sortConfig.key] > b[sortConfig.key]) {
-                return sortConfig.direction === 'ascending' ? 1 : -1;
-            }
-            return 0;
-        });
-    }
+        if (sortConfig != null) {
+            sortedData.sort((a, b) => {
+                if (a[sortConfig.key] < b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                }
+                if (a[sortConfig.key] > b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+
+        return sortedData;
+    }, [libraryData, sortConfig]);
 
     const requestSort = key => {
         let direction = 'ascending';
@@ -37,7 +41,7 @@ const Table = (props) => {
                         <th><button type="button" onClick={() => requestSort('author')}>Author</button></th>
                     </tr>
                 </thead>
-                <TableBody libraryData = {sortedData}/>
+                <TableBody libraryData = {tableData}/>
             </table>
         </React.Fragment>
     )
