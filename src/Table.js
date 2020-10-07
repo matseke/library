fa0abb76f94f94d963cea2181cf59ebda02c7fbb
Mats Ekelund <mats.ekelund@fro.se>
2020-10-07 13:27:20 +0200

    Sort direction

--- src/Table.js
+++ src/Table.js
@@ -2,28 +2,36 @@ import React from 'react';
 import TableBody from './TableBody';
 
 const Table = (props) => {
-    const [sortedField, setSortedField] = React.useState(null);
+    const [sortConfig, setSortConfig] = React.useState(null);
     const {libraryData} = props;
-    let sortedData = [...libraryData];
-    if (sortedField !== null) {
+    var sortedData = [...libraryData];
+
+    const requestSort = key => {
+        let direction = 'ascending';
+        if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+          direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+      }
+
+      if (sortConfig != null) {
         sortedData.sort((a, b) => {
-        if (a[sortedField] < b[sortedField]) {
-          return -1;
-        }
-        if (a[sortedField] > b[sortedField]) {
-          return 1;
-        }
-        return 0;
-      });
+            if (a[sortConfig.key] < b[sortConfig.key]) {
+                return sortConfig.direction === 'ascending' ? -1 : 1;
+            }
+            if (a[sortConfig.key] > b[sortConfig.key]) {
+                return sortConfig.direction === 'ascending' ? 1 : -1;
+            }
+            return 0;
+        });
     }
-   
     return(
         <React.Fragment>
             <table>
                 <thead>
                     <tr>
-                        <th><button type="button" onClick={() => setSortedField('title')}>Title</button></th>
-                        <th><button type="button" onClick={() => setSortedField('author')}>Author</button></th>
+                        <th><button type="button" onClick={() => requestSort('title')}>Title</button></th>
+                        <th><button type="button" onClick={() => requestSort('author')}>Author</button></th>
                     </tr>
                 </thead>
                 <TableBody libraryData = {sortedData}/>
